e437ec1dab378bbb6f539e0ed021a77fa79d6ee8
陈世湖 <1451801975@qq.com>
2018-08-14 17:56:42 +0800

    新增根据cookie名字获取对应cookie方法

--- src/main/java/com/csh/utils/CookieUtil.java
+++ src/main/java/com/csh/utils/CookieUtil.java
@@ -2,54 +2,50 @@ package com.csh.utils;
 
 import cn.hutool.core.collection.CollectionUtil;
 import cn.hutool.core.io.FileUtil;
-import cn.hutool.core.util.ArrayUtil;
+import cn.hutool.core.text.StrSpliter;
 import cn.hutool.core.util.CharsetUtil;
+import cn.hutool.core.util.StrUtil;
 import cn.hutool.setting.Setting;
 import org.apache.log4j.Logger;
 
-import java.io.File;
-import java.io.IOException;
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 public class CookieUtil {
 
 	private static final Logger logger = Logger.getLogger(CookieUtil.class);
 
-	public static String COOKIE_STR = "";
-
-	public static final List<String> COOKIES = new ArrayList<>();
+	public static List<String> COOKIES = new ArrayList<>();
 
 	private static final String WARN = "#请勿随意更改文件内容，导致登录状态失效！\r\n";
 
-	private static final File COOKIE_FILE = new File(System.getProperty("user.dir") + "/config/cookie.ini");
+	private static final Setting setting;
 
 	static {
-		try {
-			if (!COOKIE_FILE.exists()) {
-				COOKIE_FILE.getParentFile().mkdir();
-				COOKIE_FILE.createNewFile();
-			}
+		setting = new Setting(System.getProperty("user.dir") + "/config/cookie.ini");
+		setting.forEach((key, value) -> COOKIES.add(key + "=" + value));
+	}
 
-			new Setting(COOKIE_FILE.getAbsolutePath()).forEach((key, value) -> COOKIES.add(key + "=" + value));
-
-			COOKIE_STR = CollectionUtil.join(COOKIES, "; ");
-		} catch (IOException e) {
-			logger.error(e.getMessage(), e);
-		}
+	public static String getCookie(String name) {
+		return setting.get(name);
 	}
 
 	/**
 	 * 将Cookie保存到属性文件中
 	 *
-	 * @param cookies Cookie值，eg：token=47ce0c62f1dd483899fe0b7f8c4c9a5d
+	 * @param cookies Cookie值，eg：token=47ce0c62f1dd483899fe0b7f8c4c9a5d; name=1234
 	 */
-	public static void setCookies(String... cookies) {
-		COOKIES.clear();
-		COOKIES.addAll(CollectionUtil.toList(cookies));
-		COOKIE_STR = ArrayUtil.join(cookies, "; ");
-		FileUtil.writeString(WARN + ArrayUtil.join(cookies, "\r\n"), COOKIE_FILE, CharsetUtil.CHARSET_UTF_8);
+	public static void setCookies(String cookies) {
+		COOKIES = StrSpliter.splitTrim(cookies, "; ", true);
+
+		COOKIES.forEach(s -> {
+			if (StrUtil.isNotBlank(s) && s.contains("=")) {
+				String arr[] = s.split("=", 2);
+				setting.set(arr[0], arr[1]);
+			}
+		});
+
+		FileUtil.writeString(WARN + CollectionUtil.join(COOKIES, "\r\n"),
+				setting.getSettingPath(), CharsetUtil.CHARSET_UTF_8);
 	}
 }
